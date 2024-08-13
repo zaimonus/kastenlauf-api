9d2c7476b2469e446c0323df288fcaadf28e6ce2
zaimonus <simon.groseb@gmail.com>
2024-08-13 16:42:04 +0200

    add fixtures to populate the db

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,21 +7,69 @@ import pytest
 from dotenv import load_dotenv
 
 from app.main import app
+from app.models.checkpoint import CheckpointDocument
+from app.models.event import Arrival, EventDocument
+from app.models.team import TeamDocument
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture(scope="session", autouse=True)
 async def test_env() -> None:
     load_dotenv(".dev.env", override=True)
 
 
-@pytest.fixture()
+@pytest.fixture
 async def lifespan_app() -> AsyncGenerator[FastAPI]:
     async with LifespanManager(app) as m:
         yield m.app
 
 
-@pytest.fixture()
+@pytest.fixture
 async def client(lifespan_app: FastAPI) -> AsyncGenerator[AsyncClient]:
     transport = ASGITransport(app=lifespan_app)
     async with AsyncClient(transport=transport, base_url="http://test") as a:
         yield a
+
+
+@pytest.fixture
+async def create_team() -> AsyncGenerator[TeamDocument]:
+    team = TeamDocument(name="TestTeam", members=["Mem1", "Mem2"])
+    await team.save()
+    yield team
+    await team.delete()
+
+
+@pytest.fixture
+async def create_checkpoint() -> AsyncGenerator[CheckpointDocument]:
+    checkpoint = CheckpointDocument(
+        name="TestCheckpoint", members=["G1", "G2"]
+    )
+    await checkpoint.save()
+    yield checkpoint
+    await checkpoint.delete()
+
+
+@pytest.fixture
+async def create_event() -> AsyncGenerator[EventDocument]:
+    event = EventDocument(name="TestEvent")
+    await event.save()
+    yield event
+    await event.delete()
+
+
+@pytest.fixture
+async def create_arrival(
+    create_event: EventDocument,
+    create_team: TeamDocument,
+    create_checkpoint: CheckpointDocument,
+) -> AsyncGenerator[Arrival]:
+    arrival = Arrival(team=create_team.id, checkpoint=create_checkpoint.id)
+    event = create_event
+    event.arrivals.append(arrival)
+    await event.save()
+    yield arrival
+    await event.delete()
