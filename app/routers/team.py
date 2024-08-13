1b67d5fd42b543ad8d95bc875c49ff2f5bd3be99
zaimonus <simon.groseb@gmail.com>
2024-08-13 16:56:45 +0200

    implemented team routes [WIP]

--- app/routers/team.py
+++ app/routers/team.py
@@ -1,24 +1,35 @@
 from beanie import PydanticObjectId
-from fastapi import APIRouter
+from beanie.operators import Set
+from beanie.exceptions import DocumentNotFound
+from fastapi import APIRouter, HTTPException
 
-from app.models.team import TeamBase, TeamResponse
+from app.models.team import TeamBase, TeamDocument, TeamResponse
 
 router = APIRouter()
 
 
 @router.get("/{id}")
 async def get(id: PydanticObjectId) -> TeamResponse:
-    # TODO implement
-    pass
+    team = await TeamDocument.get(id)
+    if team is None:
+        raise HTTPException(status_code=404, detail="Team not found")
+    return team
 
 
 @router.patch("/{id}")
 async def update(id: PydanticObjectId, team: TeamBase) -> TeamResponse:
-    # TODO implement
-    pass
+    doc = await TeamDocument.get(id)
+    if doc is None:
+        raise HTTPException(status_code=404, detail="Team not found")
+    await doc.update(Set(team.model_dump(exclude_unset=True)))
+    return doc
 
 
 @router.delete("/{id}")
 async def delete(id: PydanticObjectId) -> TeamResponse:
-    # TODO implement
-    pass
+    try:
+        team = await TeamDocument.get(id)
+        await team.delete()
+        return team
+    except DocumentNotFound:
+        raise HTTPException(status_code=404, detail="Team not found")
