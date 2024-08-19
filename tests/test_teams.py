313f3cfe701ed1771ae7be36275b5dab8887ef89
zaimonus <simon.groseb@gmail.com>
2024-08-19 17:02:10 +0200

    simplified tests

--- tests/test_teams.py
+++ tests/test_teams.py
@@ -1,3 +1,4 @@
+from unittest.mock import ANY
 from beanie import PydanticObjectId
 from httpx import AsyncClient
 import pytest
@@ -8,11 +9,8 @@ from app.models.team import TeamDocument
 @pytest.mark.anyio
 async def test_get_empty_teams(client: AsyncClient) -> None:
     response = await client.get("/teams")
-
     assert response.status_code == 200
-    json = response.json()
-    assert isinstance(json, list)
-    assert len(json) == 0
+    assert response.json() == []
 
 
 @pytest.mark.anyio
@@ -20,15 +18,14 @@ async def test_get_one_team(
     client: AsyncClient, create_team: TeamDocument
 ) -> None:
     response = await client.get("/teams")
-
     assert response.status_code == 200
-    json = response.json()
-    assert isinstance(json, list)
-    assert len(json) == 1
-    obj = json[0]
-    assert obj["id"] == str(create_team.id)
-    assert obj["name"] == create_team.name
-    assert obj["member"] == create_team.member
+    assert response.json() == [
+        {
+            "id": str(create_team.id),
+            "name": create_team.name,
+            "member": create_team.member,
+        }
+    ]
 
 
 @pytest.mark.anyio
@@ -36,11 +33,11 @@ async def test_post_new_team(client: AsyncClient, cleanup_teams: None) -> None:
     name = "NewTeam"
     member = "Member1, Member2, Member3"
     data = {"name": name, "member": member}
-
     response = await client.post("/teams", json=data)
-
     assert response.status_code == 200
-    obj = response.json()
-    assert obj["name"] == name
-    assert obj["member"] == member
-    assert PydanticObjectId.is_valid(obj["id"])
+    assert response.json() == {
+        "id": ANY,
+        "name": name,
+        "member": member,
+    }
+    assert PydanticObjectId.is_valid(response.json()["id"])
